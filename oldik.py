92888cad55972e027a17ac6049a76e08c50dd690
Sourcery AI <>
2022-11-18 13:43:29 +0000

    'Refactored by Sourcery'

--- oldik.py
+++ oldik.py
@@ -27,7 +27,7 @@ VERSION_TO_FRAMES = {
 def clear_line(n=1):
     LINE_UP = '\033[1A'
     LINE_CLEAR = '\x1b[2K'
-    for i in range(n):
+    for _ in range(n):
         print(LINE_UP, end=LINE_CLEAR)
 
 
